241beb08510f5432b96d2191f1b7d07ef1dd3742
pjione <a41924250@gmail.com>
2024-06-01 20:12:36 +0900

    initial setting

---
+++ src/main/java/com/cw/restory/RestoryApplication.java
@@ -0,0 +1,13 @@
+package com.cw.restory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cw/restory/RestoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cw.restory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
