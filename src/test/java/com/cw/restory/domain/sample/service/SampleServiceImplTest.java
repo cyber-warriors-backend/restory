5df9c2e006d7d3ff287fc8b702e395f310fa3daa
pjione <a41924250@gmail.com>
2024-06-17 14:47:23 +0900

    feat : 테스트코드용 .yml 추가

--- src/test/java/com/cw/restory/domain/sample/service/SampleServiceImplTest.java
+++ src/test/java/com/cw/restory/domain/sample/service/SampleServiceImplTest.java
@@ -1,9 +1,11 @@
 package com.cw.restory.domain.sample.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class SampleServiceImplTest {
 
     @Test
