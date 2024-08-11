facbbfe39afbf76a6a6c4b8d19db208c3c7195d8
pjione <a41924250@gmail.com>
2024-08-11 15:45:33 +0900

    fix : .yml 수정

--- src/main/java/com/cw/restory/config/WebMvcConfig.java
+++ src/main/java/com/cw/restory/config/WebMvcConfig.java
@@ -1,15 +1,22 @@
 package com.cw.restory.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${server.url}")
+    private String serverUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://dev.restory.site", "https://api.restory.site")
+                .allowedOrigins(serverUrl)
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .maxAge(3600); // 1시간 동안 캐시된 CORS 설정 유지
